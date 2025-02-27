f430c994a3ae85ffff0c391421ec282b49d3be1b
DioneMartin <dione.gpe0102@gmail.com>
2025-02-26 19:08:31 -0600

    Ngrok deployment

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+
+# Detener NGINX y NGROK si están corriendo
+echo "Apagando NGINX y NGROK..."
+sudo systemctl stop nginx
+pkill -f ngrok
+
+# Ir al directorio del repositorio y actualizar la aplicación
+echo "Actualizando el repositorio..."
+cd /var/www/Resume-AWS || exit
+git pull 
+
+# Encender NGINX
+echo "Encendiendo NGINX..."
+sudo systemctl start nginx
+
+# Iniciar NGROK en el puerto 80 y obtener la URL pública
+echo "Encendiendo NROK..."
+sleep 5
+ngrok http 80
+
+
